1f7278a5729482e4c176eaf2a90f7eb7284be1e4
igorr1970 <igorr1970@mail.ru>
2024-08-18 11:28:01 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -6,4 +6,4 @@ print(age) # Ответ: 54
 age=age+1 # Перезапишите в age текущее значение переменной age + новое
 print(age) # Ответ: 55
 is_student=True # Создайте переменную is_student и присвойте ей значение True (логическое значение)
-print(True) # Ответ: True
+print(is_student) # Ответ: True
